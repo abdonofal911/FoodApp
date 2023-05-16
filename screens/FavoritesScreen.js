1085e1d31d8657c21077d550d0446fdfa5b5a971
Abdulrahman Nofal <abdonofa1712@gmail.com>
2023-05-16 12:48:54 +0300

    Changes to be committed: 	modified:   screens/FavoritesScreen.js

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -13,10 +13,8 @@ const FavoritesScreen = () => {
 
   if (favoriteMeals.length === 0) {
     return (
-      <View>
-        <Text>
-        You Have No Favorite Meals Yet. 
-        </Text>
+      <View style={styles.rootContaiter}>
+        <Text style={styles.text}>Opps You Have No Favorite Meals Yet.</Text>
       </View>
     );
   } else {
@@ -27,13 +25,14 @@ const FavoritesScreen = () => {
 export default FavoritesScreen;
 
 const styles = StyleSheet.create({
-  rootContaiter : {
-    flex: 1 , 
-    justifyContent : 'center' , 
-    alignItems : 'center'
-  }, 
-   text : {
-    fontSize : 18 , fontWeight : 'bold' , 
-    colo:'white'
-   }
+  rootContaiter: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+  },
 });
